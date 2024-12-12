67349e516005bb4e37c704cc284d43ff288246d4
Konstantinos Mokas <kostasmokas12@gmail.com>
2024-12-12 18:33:13 +0200

    Fix admin name

---
+++ src/main/java/io/github/dmgtechlabs/users/Users.java
@@ -0,0 +1,13 @@
+package io.github.dmgtechlabs.users;
+
+class Users {
+  private String name;
+
+  public Users() {
+    this.name = "";
+  }
+
+  public void login() {
+    System.out.println("User is logging in");
+  }
+}
