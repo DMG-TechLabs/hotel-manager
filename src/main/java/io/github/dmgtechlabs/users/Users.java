39ffe5782c8e9e874d67f4645320c6f83e0aab4f
Konstantinos Mokas <kostasmokas12@gmail.com>
2024-12-19 18:16:43 +0200

    apply stash

---
+++ src/main/java/io/github/dmgtechlabs/users/Users.java
@@ -0,0 +1,67 @@
+package io.github.dmgtechlabs.users;
+
+class Users {
+  private String username;
+  private String email;
+  private String password;
+  private String FirtName;
+  private String LastName;
+
+  public Users(String username, String email, String password, String FirtName, String LastName) {
+    this.username = username;
+    this.email = email;
+    this.password = password;
+    this.FirtName = FirtName;
+    this.LastName = LastName;
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public String getFirtName() {
+    return FirtName;
+  }
+
+  public void setFirtName(String FirtName) {
+    this.FirtName = FirtName;
+  }
+
+  public String getLastName() {
+    return LastName;
+  }
+
+  public void setLastName(String LastName) {
+    this.LastName = LastName;
+  }
+
+  public void login() {
+    boolean user = true;
+    if (user) {
+      this.username = "John Doe";
+      System.out.println("User is logged in");
+    } else {
+      System.out.println("User is not logged in");
+    }
+  }
+}
