f30250626936b80a749cad17cf2f88aeb10c4f3f
Konstantinos Despoinidis <kdesp2003@gmail.com>
2024-12-19 18:27:38 +0200

    added postgres driver dependency

--- src/main/java/io/github/dmgtechlabs/Main.java
+++ src/main/java/io/github/dmgtechlabs/Main.java
@@ -1,5 +1,6 @@
 package io.github.dmgtechlabs;
 
+import io.github.dmgtechlabs.models.Room;
 import io.github.kdesp73.databridge.helpers.Config;
 
 /**
@@ -8,6 +9,6 @@ import io.github.kdesp73.databridge.helpers.Config;
  */
 public class Main {
 	public static void main(String[] args) {
-		
+		new Room(1, 1, Room.Type.SINGLE, 50.2f).insert();
 	}
 }
