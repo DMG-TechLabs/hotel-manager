6776574e98f48c557fd658b751e9008668719c7d
Konstantinos Mokas <kostasmokas12@gmail.com>
2025-01-10 13:42:23 +0200

    Update user class

--- src/main/java/io/github/dmgtechlabs/db/Dao.java
+++ src/main/java/io/github/dmgtechlabs/db/Dao.java
@@ -2,21 +2,9 @@ package io.github.dmgtechlabs.db;
 
 public interface Dao {
 
-	default boolean insert() {
-		return false;
-	};
+	public boolean insert();
 
 	public boolean update(Object... values);
 
-	default boolean delete() {
-		return false;
-	};
-
-	default boolean delete(Object... values) {
-		return false;
-	};
-
-	default boolean insert(Object... values) {
-		return false;
-	};
+	public boolean delete();
 }
