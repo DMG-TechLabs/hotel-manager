3f371220467b974123fbee850c2b1e9785719d01
Constantine <konstantinosyants@gmail.com>
2025-01-10 15:22:40 +0200

    Customer

---
+++ src/main/java/io/github/dmgtechlabs/models/Customer.java
@@ -0,0 +1,177 @@
+package io.github.dmgtechlabs.models;
+
+import io.github.dmgtechlabs.Utils;
+import io.github.dmgtechlabs.db.Dao;
+import io.github.kdesp73.databridge.connections.AvailableConnections;
+import io.github.kdesp73.databridge.connections.PostgresConnection;
+import io.github.kdesp73.databridge.helpers.Adapter;
+import io.github.kdesp73.databridge.helpers.SQLogger;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Customer {
+
+    // Fields corresponding to the customer table
+    private int id;
+    private String firstName;
+    private String lastName;
+    private long phone;
+    private String email;
+
+    // Default constructor
+    public Customer() {}
+
+    // Parameterized constructor
+    public Customer(int id, String firstName, String lastName, long phone, String email) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phone = phone;
+        this.email = email;
+    }
+
+    // Getters and Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public long getPhone() {
+        return phone;
+    }
+
+    public void setPhone(long phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    // Insert a new customer
+    public boolean insert() {
+        try (PostgresConnection conn = (PostgresConnection) AvailableConnections.POSTGRES.getConnection()) {
+            conn.callFunction("insert_customer", firstName, lastName, phone, email);
+            return true;
+        } catch (SQLException e) {
+            SQLogger.getLogger().log(SQLogger.LogLevel.ERRO, "Insert Customer failed", e);
+            return false;
+        }
+    }
+
+    // Update an existing customer
+    public boolean update(Object... values) {
+        final int expectedParams = 4; // Including all fields
+        if (values.length != expectedParams)
+            throw new IllegalArgumentException(String.format("Invalid number of values (%s). Expected %d", values.length, expectedParams));
+
+        try (PostgresConnection conn = (PostgresConnection) AvailableConnections.POSTGRES.getConnection()) {
+            conn.callFunction("update_customer", id, values[0], values[1], values[2], values[3]);
+            return true;
+        } catch (SQLException e) {
+            SQLogger.getLogger().log(SQLogger.LogLevel.ERRO, "Update Customer failed", e);
+            return false;
+        }
+    }
+
+    // Delete an existing customer
+    public boolean delete() {
+        try (PostgresConnection conn = (PostgresConnection) AvailableConnections.POSTGRES.getConnection()) {
+            conn.callFunction("delete_customer", id);
+            return true;
+        } catch (SQLException e) {
+            SQLogger.getLogger().log(SQLogger.LogLevel.ERRO, "Delete Customer failed", e);
+            return false;
+        }
+    }
+
+    // Select customers by id
+    public static List<Customer> selectById(int id) {
+        return select("select_customer_by_id", id);
+    }
+
+    // Select customers by email
+    public static List<Customer> selectByEmail(String email) {
+        return select("select_customer_by_email", email);
+    }
+
+    // Helper method for selection queries
+    private static List<Customer> select(String function, Object... values) {
+        List<Customer> customers = new ArrayList<>();
+        try (PostgresConnection conn = (PostgresConnection) AvailableConnections.POSTGRES.getConnection()) {
+            ResultSet rs = conn.callFunction(function, values);
+            while (rs.next()) {
+                Customer customer = new Customer();
+                customer.setId(rs.getInt("id"));
+                customer.setFirstName(rs.getString("fname"));
+                customer.setLastName(rs.getString("lname"));
+                customer.setPhone(rs.getLong("phone"));
+                customer.setEmail(rs.getString("email"));
+                customers.add(customer);
+            }
+            rs.close();
+        } catch (SQLException e) {
+            SQLogger.getLogger().log(SQLogger.LogLevel.ERRO, "Select " + function + " failed", e);
+        }
+        return customers;
+    }
+
+    // Select all customers
+    public static List<Customer> selectAll() {
+        List<Customer> customers = new ArrayList<>();
+        try (PostgresConnection conn = (PostgresConnection) AvailableConnections.POSTGRES.getConnection()) {
+            ResultSet rs = conn.callFunction("select_all_customers");
+            while (rs.next()) {
+                Customer customer = new Customer();
+                customer.setId(rs.getInt("id"));
+                customer.setFirstName(rs.getString("fname"));
+                customer.setLastName(rs.getString("lname"));
+                customer.setPhone(rs.getLong("phone"));
+                customer.setEmail(rs.getString("email"));
+                customers.add(customer);
+            }
+            rs.close();
+        } catch (SQLException e) {
+            SQLogger.getLogger().log(SQLogger.LogLevel.ERRO, "Select All Customers failed", e);
+        }
+        return customers;
+    }
+
+    // toString method
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", phone=" + phone +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
