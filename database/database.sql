d51d22417bf38eb0981b9613db2fe32f43b8d8ef
Constantine <konstantinosyants@gmail.com>
2025-01-10 13:48:30 +0200

    Insert procedures Postgress

--- database/database.sql
+++ database/database.sql
@@ -54,3 +54,111 @@ CREATE TABLE RESERVATION (
     FOREIGN KEY (customer_id) REFERENCES CUSTOMER (customer_id),
     FOREIGN KEY (room_id) REFERENCES ROOM (room_id)
 );
+
+--Insert Proceedures
+CREATE OR REPLACE FUNCTION insert_system_user(
+    _username VARCHAR,
+    _password VARCHAR,
+    _user_type INT,
+    _hotel_id INT DEFAULT NULL
+) RETURNS VOID AS $$
+BEGIN
+    BEGIN
+        INSERT INTO SYSTEM_USER (username, password, user_type, hotel_id)
+        VALUES (_username, _password, _user_type, _hotel_id);
+    EXCEPTION WHEN unique_violation THEN
+        RAISE EXCEPTION 'Username % already exists', _username;
+    WHEN foreign_key_violation THEN
+        RAISE EXCEPTION 'Hotel ID % or User Type % does not exist', _hotel_id, _user_type;
+    WHEN others THEN
+        RAISE EXCEPTION 'An unexpected error occurred while inserting SYSTEM_USER.';
+    END;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION insert_customer(
+    _first_name VARCHAR,
+    _last_name VARCHAR,
+    _phone BIGINT,
+    _email VARCHAR
+) RETURNS VOID AS $$
+BEGIN
+    BEGIN
+        INSERT INTO CUSTOMER (first_name, last_name, phone, email)
+        VALUES (_first_name, _last_name, _phone, _email);
+    EXCEPTION WHEN others THEN
+        RAISE EXCEPTION 'An error occurred while inserting CUSTOMER.';
+    END;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION insert_hotel(
+    _name VARCHAR,
+    _address VARCHAR,
+    _phone BIGINT
+) RETURNS VOID AS $$
+BEGIN
+    BEGIN
+        INSERT INTO HOTEL (name, address, phone)
+        VALUES (_name, _address, _phone);
+    EXCEPTION WHEN others THEN
+        RAISE EXCEPTION 'An error occurred while inserting HOTEL.';
+    END;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION insert_room(
+    _number INT,
+    _hotel_id INT,
+    _type INT,
+    _price NUMERIC(10, 2),
+    _floor INT,
+    _occupied BOOLEAN DEFAULT FALSE
+) RETURNS VOID AS $$
+BEGIN
+    BEGIN
+        INSERT INTO ROOM (number, hotel_id, type, price, floor, occupied)
+        VALUES (_number, _hotel_id, _type, _price, _floor, _occupied);
+    EXCEPTION WHEN foreign_key_violation THEN
+        RAISE EXCEPTION 'Hotel ID % or Room Type % does not exist', _hotel_id, _type;
+    WHEN others THEN
+        RAISE EXCEPTION 'An error occurred while inserting ROOM.';
+    END;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION insert_amenities(
+    _hotel_id INT,
+    _type VARCHAR
+) RETURNS VOID AS $$
+BEGIN
+    BEGIN
+        INSERT INTO AMENITIES (hotel_id, type)
+        VALUES (_hotel_id, _type);
+    EXCEPTION WHEN foreign_key_violation THEN
+        RAISE EXCEPTION 'Hotel ID % does not exist', _hotel_id;
+    WHEN others THEN
+        RAISE EXCEPTION 'An error occurred while inserting AMENITIES.';
+    END;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION insert_reservation(
+    _customer_id INT,
+    _room_id INT,
+    _check_in_date DATE,
+    _check_out_date DATE,
+    _cost NUMERIC(10, 2),
+    _status INT
+) RETURNS VOID AS $$
+BEGIN
+    BEGIN
+        INSERT INTO RESERVATION (customer_id, room_id, check_in_date, check_out_date, cost, status)
+        VALUES (_customer_id, _room_id, _check_in_date, _check_out_date, _cost, _status);
+    EXCEPTION WHEN foreign_key_violation THEN
+        RAISE EXCEPTION 'Customer ID % or Room ID % does not exist', _customer_id, _room_id;
+    WHEN others THEN
+        RAISE EXCEPTION 'An error occurred while inserting RESERVATION.';
+    END;
+END;
+$$ LANGUAGE plpgsql;
